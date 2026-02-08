9f2221483b0cf072f345df8fac61678217292a1d
Raj Sinthalapadi <raj@mahima.info>
2026-02-08 17:51:26 +1100

    Configure Azure backend storage for Terraform state

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+  
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstatestg007"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
